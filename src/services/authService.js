4256709b28ae409da2d58ba70065cefd65e04ad9
MOHIT-M-PATEL <124170169+MOHIT-M-PATEL@users.noreply.github.com>
2024-04-26 10:15:41 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 
 const authService = {
     checkAuth: () => {
-        return axios.get('http://localhost:5000/checkAuth', { withCredentials: true });
+        return axios.get('https://mern-online-b4p1.onrender.com/checkAuth', { withCredentials: true });
     },
     login: (username, password) => {
-        return axios.post('http://localhost:5000/login', { username, password }, { withCredentials: true });
+        return axios.post('https://mern-online-b4p1.onrender.com/login', { username, password }, { withCredentials: true });
     },
     signup: (username, password) => {
-        return axios.post('http://localhost:5000/signup', { username, password }, { withCredentials: true });
+        return axios.post('https://mern-online-b4p1.onrender.com/signup', { username, password }, { withCredentials: true });
     },
     logout: () => {
-        return axios.post('http://localhost:5000/logout', {}, { withCredentials: true });
+        return axios.post('https://mern-online-b4p1.onrender.com/logout', {}, { withCredentials: true });
     }
 };
 
